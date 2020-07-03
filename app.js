6a0c4c3a3ad1402445b058909f8364960d259107
Shubham Sharma <s.psharma887@gmail.com>
2020-07-03 11:37:18 +0530

    passport

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 
 const mongoose = require("mongoose");
-
-const Dishes = require("./models/dishes");
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 const url = "mongodb://localhost:27017/confusion";
 const connect = mongoose.connect(url);
@@ -48,25 +48,21 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 //basic authentication middlewere
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 app.use(auth);
