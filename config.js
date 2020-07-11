0899f0f9c9e146e1f2ea7d966e44fd092cd47fdf
Shubham Sharma <s.psharma887@gmail.com>
2020-07-11 17:56:38 +0530

    oauth facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/confusion",
+  facebook: {
+    clientId: "702860570507922",
+    clientSecret: "d8ff2a268e896c25e0e74757e75d375c",
+  },
 };
